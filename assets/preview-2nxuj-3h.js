1ce70461fa82930df30da99d14f200844b350acb
ssbarbee <ssbarbee@users.noreply.github.com>
2024-07-19 15:49:16 +0200

    Updates

---
+++ assets/preview-2nxuj-3h.js
@@ -0,0 +1,1 @@
+const a={parameters:{backgrounds:{default:"discord",values:[{name:"discord",value:"#313338"}]},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}}}};export{a as default};
