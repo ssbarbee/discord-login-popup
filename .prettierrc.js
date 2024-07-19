85a3559b2590cdf282b21b523b1199374f0b0d10
ssbarbee <ssbarbee@users.noreply.github.com>
2024-07-19 10:58:39 +0200

    feat: add eslint, prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
--- src/stories/react-integration/ReactIntegration.tsx
+++ src/stories/react-integration/ReactIntegration.tsx
@@ -1,45 +1,66 @@
-import React from 'react';
 import './react-integration.css';
+
+import React from 'react';
+
 import { discordLoginPopup } from '../../index';
 import { DiscordIcon } from './DiscordIcon';
 
 type ReactIntegrationProps = {
-    discordAppClientId: string;
-    redirectUrl: string;
-    scopes: string;
+  discordAppClientId: string;
+  redirectUrl: string;
+  scopes: string;
 };
 
 export const ReactIntegration = ({
-    discordAppClientId, redirectUrl, scopes = 'identify'
+  discordAppClientId,
+  redirectUrl,
+  scopes = 'identify',
 }: ReactIntegrationProps) => {
-    if(!discordAppClientId) {
-        throw new Error('For this story to work, discordAppClientId is required');
-    }
-    if(!redirectUrl) {
-        throw new Error('For this story to work, redirectUrl is required');
-    }
+  const [token, setToken] = React.useState<string>('');
+  const [error, setError] = React.useState<string>('');
+  const [loading, setLoading] = React.useState<boolean>(false);
 
-    const loginWithDiscord = () => {
-        discordLoginPopup({
-            discordAppClientId,
-            redirectUrl, scopes,
-            onSuccess: (data) => {
-                console.log(data);
-            },
-            onError: (data) => {
-                console.log(data);
-            },
-            onClose: () => {
-                console.log('closed');
-            },
-            onStart: () => {
-                console.log('started');
-            }
-        })
-    };
+  if (!discordAppClientId) {
+    throw new Error('For this story to work, discordAppClientId is required');
+  }
+  if (!redirectUrl) {
+    throw new Error('For this story to work, redirectUrl is required');
+  }
+
+  const loginWithDiscord = () => {
+    discordLoginPopup({
+      discordAppClientId,
+      onClose: () => {},
+      onError: data => {
+        setToken('');
+        setError(data.error);
+        setLoading(false);
+      },
+      onStart: () => {
+        setLoading(true);
+      },
+      onSuccess: data => {
+        setToken(data.token);
+        setError('');
+        setLoading(false);
+      },
+      redirectUrl,
+      scopes,
+    });
+  };
   return (
-    <button className='storybook-button' onClick={loginWithDiscord}>
-      <DiscordIcon /> Login with Discord
-    </button>
+    <div className="storybook-button-container">
+      <button className="storybook-button" onClick={loginWithDiscord}>
+        {loading ? 'Loading...' : ''}
+        {!loading && (
+          <React.Fragment>
+            <DiscordIcon />
+            Login with Discord
+          </React.Fragment>
+        )}
+      </button>
+      {error && <div className="storybook-error">{error}</div>}
+      {token && <div className="storybook-token">{token}</div>}
+    </div>
   );
 };
