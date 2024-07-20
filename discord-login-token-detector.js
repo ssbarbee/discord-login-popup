5e0cec3d4d3b12e8cfdbd999e08ded82b0bc4cca
ssbarbee <ssbarbee@users.noreply.github.com>
2024-07-20 17:55:54 +0200

    Updates

--- discord-login-token-detector.js
+++ discord-login-token-detector.js
@@ -1,11 +1,15 @@
-window.addEventListener('message', (event) => {
-  // Remove the leading #
-  var params = new URLSearchParams(window.location.hash.substring(1));
-  var token = params.get('access_token');
-  if (token) {
+function parseExpiredIn(value) {
+  var parsed = parseInt(value, 10);
+  return isNaN(parsed) ? null : parsed;
+}
+
+function checkForCode(event) {
+  var params = new URLSearchParams(window.location.search);
+  var code = params.get('code');
+  if (code) {
     event.source.postMessage(
       {
-        token: token,
+        code: code,
       },
       event.origin,
     );
@@ -14,10 +18,56 @@ window.addEventListener('message', (event) => {
     if (error) {
       event.source.postMessage(
         {
-          error: error,
+          error: params.get('error'),
+          error_description: params.get('error_description'),
         },
         event.origin,
       );
     }
+  }
+}
+
+// https://discord.com/developers/docs/topics/oauth2#implicit-grant
+function checkForToken(event) {
+  // Remove the leading #
+  var params = new URLSearchParams(window.location.hash.substring(1));
+  var access_token = params.get('access_token');
+  if (access_token) {
+    var expires_in = parseExpiredIn(params.get('expires_in'));
+    var token_type = params.get('token_type');
+    var scope = params.get('scope');
+    event.source.postMessage(
+      {
+        access_token,
+        expires_in,
+        token_type,
+        scope,
+      },
+      event.origin,
+    );
+  } else {
+    var error = params.get('error_description') || params.get('error');
+    if (error) {
+      event.source.postMessage(
+        {
+          error: params.get('error'),
+          error_description: params.get('error_description'),
+        },
+        event.origin,
+      );
+    }
+  }
+}
+
+window.addEventListener('message', event => {
+  if (event.data.source !== 'discord-login-popup') {
+    return;
+  }
+
+  if (event.data.params.responseType === 'code') {
+    checkForCode(event);
+  }
+  if (event.data.params.responseType === 'token') {
+    checkForToken(event);
   }
 });
--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,17 @@
 export type OnErrorParams = {
     error: string;
+    error_description: string;
 };
-export type OnSuccessParams = {
-    token: string;
+export type OnSuccessTokenParams = {
+    access_token: string;
+    expires_in: number;
+    scope: string;
+    token_type: string;
 };
+export type OnSuccessCodeParams = {
+    code: string;
+};
+export type OnSuccessParams = OnSuccessTokenParams | OnSuccessCodeParams;
 export type DiscordLoginPopupParams = {
     discordAppClientId: string;
     onClose?: () => void;
@@ -13,6 +21,7 @@ export type DiscordLoginPopupParams = {
     popupHeight?: number;
     popupWidth?: number;
     redirectUrl: string;
+    responseType?: 'token' | 'code';
     scopes?: string;
 };
-export declare const discordLoginPopup: ({ popupWidth, popupHeight, redirectUrl, scopes, onStart, onError, onSuccess, discordAppClientId, onClose, }: DiscordLoginPopupParams) => void;
+export declare const discordLoginPopup: ({ popupWidth, popupHeight, redirectUrl, scopes, onStart, onError, onSuccess, discordAppClientId, onClose, responseType, }: DiscordLoginPopupParams) => void;
