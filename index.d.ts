bf29171546758244b8bb42a2e7b14b72866756d1
ssbarbee <ssbarbee@users.noreply.github.com>
2024-07-22 10:12:34 +0200

    Updates

--- index.d.ts
+++ index.d.ts
@@ -1,27 +1,2 @@
-export type OnErrorParams = {
-    error: string;
-    error_description: string;
-};
-export type OnSuccessTokenParams = {
-    access_token: string;
-    expires_in: number;
-    scope: string;
-    token_type: string;
-};
-export type OnSuccessCodeParams = {
-    code: string;
-};
-export type OnSuccessParams = OnSuccessTokenParams | OnSuccessCodeParams;
-export type DiscordLoginPopupParams = {
-    discordAppClientId: string;
-    onClose?: () => void;
-    onError?: (data: OnErrorParams) => void;
-    onStart?: () => void;
-    onSuccess?: (data: OnSuccessParams) => void;
-    popupHeight?: number;
-    popupWidth?: number;
-    redirectUrl: string;
-    responseType?: 'token' | 'code';
-    scopes?: string;
-};
-export declare const discordLoginPopup: ({ popupWidth, popupHeight, redirectUrl, scopes, onStart, onError, onSuccess, discordAppClientId, onClose, responseType, }: DiscordLoginPopupParams) => void;
+export { discordLoginPopup } from './lib';
+export * from './lib/types';
---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+import { DiscordLoginPopupParams } from './types';
+
+export declare const discordLoginPopup: ({ popupWidth, popupHeight, redirectUrl, scopes, onStart, onError, onSuccess, discordAppClientId, onClose, responseType, }: DiscordLoginPopupParams) => void;
